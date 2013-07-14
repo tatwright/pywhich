77f06780b26c3e13916f4f2ebc6ed31ccf707f2b
tat.wright <tat.wright@google.com>
2013-07-14 21:12:20 +0100

    Add thank you note

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ from distutils.core import setup
 setup(
     name='pywhich',
     version='1.1',
-    description='Find where Python modules are installed in the current Python',
-    long_description='Code is maintained at: https://github.com/tatwright/pywhich',
+    description='Find where python modules are installed.',
+    long_description='''\
+Thanks to Mark Paschal for taking my 10 line script and adding various useful flags, and dealing with some edge cases.
+
+Code is maintained at: https://github.com/tatwright/pywhich''',
     py_modules=['pywhich'],
     scripts=['bin/pywhich'],
     author='Mark Paschal',
